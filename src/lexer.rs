8d503453ec5b79b75bc489b5783b46f18c653493
crahs8 <lasse.moeldrup@gmail.com>
2021-03-14 20:35:26 +0100

    Refactor lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,61 +12,8 @@ struct Tokenizer<'a> {
 }
 
 impl<'a> Tokenizer<'a> {
-    fn new(program: &'a str) -> Self {
-        Tokenizer {
-            remaining_text: program,
-        }
-    }
-
     fn skip_whitespace(&mut self) {
         self.remaining_text = self.remaining_text.trim_start()
-    }
-
-    fn tokenize_char(&mut self) -> Option<Token> {
-        let first_char = self.remaining_text.chars().next();
-        match first_char {
-            Some(ch) if is_separator(ch) => {
-                self.skip_char();
-                Some(Token::Separator(ch))
-            },
-            Some('\\') => {
-                self.skip_char();
-                Some(Token::Lambda)
-            },
-            Some('=') => {
-                self.skip_char();
-                Some(Token::Macro)
-            },
-            _ => None
-        }
-    }
-
-    fn skip_char(&mut self) {
-        self.remaining_text = self.remaining_text.char_indices()
-            .nth(1)
-            .and_then(|(i, _)| self.remaining_text.get(i..))
-            .unwrap_or("");
-    }
-
-    fn tokenize_id(&mut self) -> Option<Token> {
-        if self.remaining_text.is_empty() {
-            return None;
-        }
-
-        let mut end_index= self.remaining_text.len();
-        for (index, ch) in self.remaining_text.char_indices() {
-            if ch == '\\' || is_separator(ch) || ch.is_whitespace() || ch == '=' {
-                end_index = index;
-                break;
-            }
-        }
-
-        let lexeme = &self.remaining_text[..end_index];
-
-        self.remaining_text = &self.remaining_text.get(end_index..)
-            .unwrap_or("");
-
-        Some(Token::Id(lexeme.to_owned()))
     }
 }
 
@@ -76,54 +23,40 @@ impl<'a> Iterator for Tokenizer<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         self.skip_whitespace();
 
-        self.tokenize_char()
-            .or_else(|| self.tokenize_id())
+        if self.remaining_text.is_empty() {
+            return None;
+        }
+
+        let mut lexeme_len = self.remaining_text.chars()
+            .take_while(|&c| !(is_reserved(c) || c.is_whitespace()))
+            .count();
+        if lexeme_len == 0 {
+            lexeme_len += 1;
+        }
+        let lexeme = &self.remaining_text[..lexeme_len];
+        self.remaining_text = &self.remaining_text[lexeme_len..];
+
+        Some(tokenize_lexeme(lexeme))
     }
 }
 
-fn is_separator(ch: char) -> bool {
-    ch == '(' || ch == ')' || ch == '.' || ch == ';'
+fn is_reserved(ch: char) -> bool {
+    ch == '(' || ch == ')' || ch == '.' || ch == ';' || ch == '\\' || ch == '='
 }
 
+fn tokenize_lexeme(lexeme: &str) -> Token {
+    match lexeme {
+        "(" | ")" | "." | ";" => Token::Separator(lexeme.chars().next().unwrap()),
+        "\\" => Token::Lambda,
+        "=" => Token::Macro,
+        _ => Token::Id(lexeme.to_owned()),
+    }
+}
 
 pub fn tokenize(program: &str) -> Vec<Token> {
-    Tokenizer::new(program).collect()
-}
-
-#[test]
-fn test_tokenize_char() {
-    let mut tokenizer = Tokenizer::new("(");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Separator('(')));
-
-    tokenizer = Tokenizer::new(")");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Separator(')')));
-
-    tokenizer = Tokenizer::new(".");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Separator('.')));
-
-    tokenizer = Tokenizer::new(";");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Separator(';')));
-
-    tokenizer = Tokenizer::new("\\");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Lambda));
-
-    tokenizer = Tokenizer::new("=");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Macro));
-
-    tokenizer = Tokenizer::new("\\x.x");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Lambda));
-
-    tokenizer = Tokenizer::new(".abc.");
-    assert_eq!(tokenizer.tokenize_char(), Some(Token::Separator('.')));
-}
-
-#[test]
-fn test_tokenize_id() {
-    let mut tokenizer = Tokenizer::new("abc");
-    assert_eq!(tokenizer.tokenize_id(), Some(Token::Id("abc".to_owned())));
-
-    tokenizer = Tokenizer::new("abc(de)");
-    assert_eq!(tokenizer.tokenize_id(), Some(Token::Id("abc".to_owned())));
+    Tokenizer {
+        remaining_text: program
+    }.collect()
 }
 
 #[test]
